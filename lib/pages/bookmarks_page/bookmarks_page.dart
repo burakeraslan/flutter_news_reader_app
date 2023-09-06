316ae570d5cfee96be74f3d64144b01475fb03db
huseyin.sahinli <huseyinsahinli01@gmail.com>
2023-09-06 10:14:54 +0300

    Bookmars Page Update

--- lib/pages/bookmarks_page/bookmarks_page.dart
+++ lib/pages/bookmarks_page/bookmarks_page.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_news_reader_app/models/news_model.dart';
+import 'package:flutter_news_reader_app/pages/bookmarks_page/bookmarks_page_controller.dart';
 import 'package:flutter_news_reader_app/widgets/header.dart';
 import 'package:flutter_news_reader_app/widgets/item.dart';
-import 'package:get_storage/get_storage.dart';
+import 'package:get/get.dart';
 
 class BookmarksPage extends StatelessWidget {
   const BookmarksPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final box = GetStorage();
-    List<dynamic> bookmarks = box.read("bookmarks") ?? [];
-    List<Article> bookmarkedArticles = bookmarks.map((e) => Article.fromJson(e)).toList();
-
+    Get.put(
+      BookmarsPageController(),
+    );
     return Scaffold(
       appBar: const Header(),
       backgroundColor: Colors.white,
-      body: ListView.builder(
-        itemCount: bookmarkedArticles.length,
-        itemBuilder: (context, index) {
-          final article = bookmarkedArticles[index];
-          if (article.urlToImage == null || article.urlToImage!.isEmpty) return const SizedBox();
-          return Item(article: article);
+      body: GetBuilder<BookmarsPageController>(
+        builder: (_) {
+          return Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              for (int i = 0; i < _.bookmarkedArticles.length; i++) ...[
+                Item(
+                  article: _.bookmarkedArticles[i],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ]
+            ],
+          );
         },
       ),
     );
